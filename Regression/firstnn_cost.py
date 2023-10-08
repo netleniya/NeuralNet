48fe299c2f446980cd084639459e397148e85e10
Peter Fiee <peterfiee@icloud.com>
2023-10-08 02:14:03 -0400

    Implement derivative of error function.

---
+++ Regression/firstnn_cost.py
@@ -0,0 +1,31 @@
+w = 0.1
+learning_rate = 0.1
+epochs = 10
+
+inputs = [1, 2, 3, 4]
+targets = [2, 4, 6, 8]
+
+
+def predict(input: int | float) -> int | float:
+    return w * input
+
+
+if __name__ == "__main__":
+    # train the network
+    for _ in range(epochs):
+        pred = [predict(i) for i in inputs]
+        errors = [(t - p) ** 2 for p, t in zip(pred, targets)]
+        cost = sum(errors) / len(targets)
+        print(f"Weight: {w:.2f}, Cost: {cost:.2f}")
+
+        # take the derivative of the error function
+        errors_deriv = [2 * (p - t) for p, t in zip(pred, targets)]
+        weight_delta = [e * i for e, i in zip(errors_deriv, inputs)]
+        w -= learning_rate * sum(weight_delta) / len(weight_delta)
+
+    # test the network
+    test_inputs = [5, 6]
+    test_targets = [10, 12]
+    pred = [predict(i) for i in test_inputs]
+    for i, t, p in zip(test_inputs, test_targets, pred):
+        print(f"input: {i}, target: {t}, pred: {p:.4f}")
