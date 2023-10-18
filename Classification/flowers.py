0cc7c08eca9b97530e81afedca8d300882c2e260
Peter Fiee <peterfiee@icloud.com>
2023-10-18 13:02:35 -0400

    intial commit

---
+++ Classification/flowers.py
@@ -0,0 +1,11 @@
+import flowersdata as data
+
+weights = [[0.1, 0.2], [0.15, 0.25], [0.18, 0.1]]
+biases = [0.3, 0.4, 0.35]
+epochs = 1
+learning_rate = 0.1
+
+
+# train
+for epoch in range(epochs):
+    ...
